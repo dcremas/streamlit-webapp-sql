1d7310fae7f7898b371e6e8de7910a797e32695d
Dustin Cremascoli <dustincremascoli@aol.com>
2026-01-09 13:16:37 -0600

    Updating the logic of env variables for cloud platform.

--- shared_funcs.py
+++ shared_funcs.py
@@ -1,15 +1,20 @@
+import os
+from dotenv import load_dotenv
 
-def database_path(curr_db='finance'):
-    from configparser import ConfigParser
 
-    config = ConfigParser()
-    config.read('config.ini')
+def database_path(curr_db='apple_weatherkit'):
 
-    return (
-        f"{config[curr_db]['dialect']}+{config[curr_db]['driver']}"
-        f"://{config[curr_db]['username']}:{config[curr_db]['password']}"
-        f"@{config[curr_db]['host']}:{config[curr_db]['port']}/{curr_db}"
-    )
+    load_dotenv()
+
+    dialect = os.getenv('dialect')
+    driver = os.getenv('driver')
+    username = os.getenv('username')
+    password = os.getenv('password')
+    host = os.getenv('host')
+    port = os.getenv('port')
+    curr_db = curr_db
+
+    return f"{dialect}+{driver}://{username}:{password}@{host}:{port}/{curr_db}"
 
 
 def connection_uri(curr_db='finance'):
